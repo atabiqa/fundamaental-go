e117172c2114f0496345f4140ea67381ba7becbf
atabiq <atabiqasadlearn@gmail.com>
2023-10-22 20:19:23 +0700

    create file

---
+++ op-math/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var result = 6 + 6
+	fmt.Println(result)
+
+	var a = 7
+	var b = 99
+	var c = a + b
+	fmt.Println(c)
+	//augmented assign
+	var i = 10
+	i += 10
+
+	fmt.Println(i)
+}
